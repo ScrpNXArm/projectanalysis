561918806ded3968993f9fbe84f25f716d716d95
ScrpNXArm <armanyazid50@gmail.com>
2026-06-27 14:39:46 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 // Supabase Configuration
 // Dapatkan URL dan Anon/Publishable Key dari: Supabase Dashboard > Project Settings > API
 
-const SUPABASE_URL = 'https://jnwtvnngphtokidecji.supabase.co'; // contoh: https://xxxxxxxxxxxx.supabase.co
+const SUPABASE_URL = 'https://jnwtvnngphtokidecjzi.supabase.co'; // contoh: https://xxxxxxxxxxxx.supabase.co
 const SUPABASE_ANON_KEY = 'sb_publishable_5NlEaxyMVdQ8_z5P5rL8Tw_iM2ZS2Hc'; // anon/public key (selamat untuk browser)
 
 // Initialize Supabase Client
